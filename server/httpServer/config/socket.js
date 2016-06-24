c6aff9b5db671f8cccbfa0604c0274d6adefc149
Leo Adelstein <leoadelstein@gmail.com>
2016-06-24 16:59:32 -0700

    added socket server logic

---
+++ server/httpServer/config/socket.js
@@ -0,0 +1,26 @@
+const socketIO = require('socket.io');
+
+
+// Start the socket connection server-side
+const makeSocketServer = function socketServer(http) {
+  const io = socketIO(http);
+
+  io.on('connection', (socket) => {
+    const updateUserLocation = function updateUserLocation(userId, lat, lng) {
+      const geocode = 1010;
+      process.send({ task: 'location', userId, geocode });
+    };
+
+    const updateUserShape = function updateUserShape(userId, shape) {
+      process.send({ task: 'shape', userId, shape });
+    };
+
+
+    socket.on('update location', updateUserLocation);
+    socket.on('update shape', updateUserShape);
+  });
+};
+
+module.exports = {
+  makeSocketServer,
+};
