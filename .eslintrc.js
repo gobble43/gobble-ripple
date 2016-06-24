45d2acb325f60498ae8a329d2939994bcce02c7f
Leo Adelstein <leoadelstein@gmail.com>
2016-06-23 19:47:36 -0700

    updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
     "extends": "airbnb",
+    "installedESLint": true,
     "plugins": [
         "react"
-    ]
+    ],
+    "rules": {
+    "no-console": 0,  // allow use of console (off by default in the node environment)
+    "no-undef": 0, // allow undefined 'describe' function from mocha
+    "global-require": 0, // needed for cluster architecture
+  }
 };
