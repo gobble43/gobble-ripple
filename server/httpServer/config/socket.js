09de4e237f1369a0e0bbdcabd4185b6ac7a7b37c
Leo Adelstein <leoadelstein@gmail.com>
2016-06-29 16:25:28 -0700

    socket server now takes new ripples from redis queue and distributes the ripple shape and position to all connected sockets

--- server/httpServer/config/socket.js
+++ server/httpServer/config/socket.js
@@ -1,9 +1,44 @@
 const socketIO = require('socket.io');
 const geo = require('../../../lib/geo.js');
 
+const Promise = require('bluebird');
+const redis = require('redis');
+Promise.promisifyAll(redis.RedisClient.prototype);
+Promise.promisifyAll(redis.Multi.prototype);
+const redisClient = redis.createClient();
+
 // Start the socket connection server-side
 const makeSocketServer = function socketServer(http) {
   const io = socketIO(http);
+
+  // sockets worker job is to distribute new posts to all listening sockets
+  const workerJob = () => {
+  // process one post at a time
+    const workerLoop = () => {
+      redisClient.llenAsync('ripples')
+        .then((length) => {
+          if (length === 0) {
+            setTimeout(workerLoop, 1000);
+          } else {
+            redisClient.rpopAsync('ripples')
+              .then((ripple) => {
+                io.emit('new ripple', ripple);
+                workerLoop();
+              })
+              .catch((err) => {
+                console.error(err);
+                workerLoop();
+              });
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    };
+    workerLoop();
+  };
+  // start the worker
+  workerJob();
 
   io.on('connection', (socket) => {
     const updateUserLocation = function updateUserLocation(userId, lat, lng) {
