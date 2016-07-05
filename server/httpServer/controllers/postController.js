12b3b450b9873684b4b8e7dd3fb13f63f799a85b
Leo Adelstein <leoadelstein@gmail.com>
2016-07-05 15:24:28 -0700

    implemented function to get all users ripple posts from gobble-db

--- server/httpServer/controllers/postController.js
+++ server/httpServer/controllers/postController.js
@@ -4,13 +4,39 @@ Promise.promisifyAll(redis.RedisClient.prototype);
 Promise.promisifyAll(redis.Multi.prototype);
 const redisClient = redis.createClient();
 
+// Load environment variables
+const dotenv = require('dotenv');
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config({ path: './env/development.env' });
+} else if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: './env/production.env' });
+}
+
+const fetch = require('isomorphic-fetch');
+const gobbleDBUrl = process.env.GOBBLE_DB_URL;
+
 module.exports = {
   getPosts: (userId, startingPoint, res) => {
-    console.log(typeof userId);
     redisClient.smembersAsync(userId)
       .then((posts) => {
-        res.end(JSON.stringify(posts));
-        // need to get all json contents of posts and return them
+        fetch(`${gobbleDBUrl}/db/postsById?posts=${JSON.stringify(posts)}`, {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        })
+        .then(response => {
+          console.log(`response status ${response.status}`);
+          return response.json();
+        })
+        .then(response => {
+          res.end(JSON.stringify(response));
+        })
+        .catch(err => {
+          console.err(err);
+          res.end(err);
+        });
       })
       .catch((err) => {
         console.error(err);
