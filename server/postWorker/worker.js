d46e1ff9061e3416225f26af140496b82a9c775c
Leo Adelstein <leoadelstein@gmail.com>
2016-07-06 17:07:27 -0700

    refactor

--- server/postWorker/worker.js
+++ server/postWorker/worker.js
@@ -5,109 +5,13 @@ Promise.promisifyAll(redis.Multi.prototype);
 const redisClient = redis.createClient();
 
 const geo = require('../../lib/geo.js');
-
-// get geohahing bit length / resolution for given radius in meters
-const getResolution = (size) => {
-  if (size === 'small') {
-    return 35; // 50m x 50m box
-  } else if (size === 'medium') {
-    return 30; // 300m x 300m box
-  } else if (size === 'large') {
-    return 25; // 2km x 2km box
-  }
-  return 50;
-};
-
-const addMetersToLatLng = (lng, dx, lat, dy) => {
-  const radiusOfEarth = 6371000;
-  const newLat = lat + (dy / radiusOfEarth) * (180 / Math.PI);
-  const newLng = lng + (dx / radiusOfEarth) * (180 / Math.PI / Math.cos(lat * Math.PI / 180));
-  return [newLat, newLng];
-};
-
-// get all points of a polygon bases on a center point size and shape
-const getShapePoints = (shape, center, size) => {
-  const pointsArray = [];
-  let radius = 0;
-  if (size === 'small') {
-    radius = 25;
-  } else if (size === 'medium') {
-    radius = 150;
-  } else if (size === 'large') {
-    radius = 1000;
-  }
-  if (shape === 'square') {
-    // upper left
-    pointsArray.push(addMetersToLatLng(center[1], -radius, center[0], radius));
-    // upper right
-    pointsArray.push(addMetersToLatLng(center[1], radius, center[0], radius));
-    // bottom left
-    pointsArray.push(addMetersToLatLng(center[1], -radius, center[0], -radius));
-    // bottom right
-    pointsArray.push(addMetersToLatLng(center[1], radius, center[0], -radius));
-  } else if (shape === 'triangle') {
-    // upper right
-    pointsArray.push(addMetersToLatLng(center[1], 0, center[0], radius));
-    // bottom left
-    pointsArray.push(addMetersToLatLng(center[1], -radius, center[0], -radius));
-    // bottom right
-    pointsArray.push(addMetersToLatLng(center[1], radius, center[1], radius));
-  }
-  return pointsArray;
-};
-
-// used for optimization since we are using the same set of points everytime
-const cachePoly = (polyPoints) => {
-  const constant = [];
-  const multiple = [];
-  let j = polyPoints.length - 1;
-  for (let i = 0; i < polyPoints.length; i++) {
-    if (polyPoints[j][0] === polyPoints[i][0]) {
-      constant[i] = polyPoints[i][1];
-      multiple[i] = 0;
-    } else {
-      constant[i] = polyPoints[i][1] - (polyPoints[i][0] * polyPoints[j][1]) /
-        (polyPoints[j][0] - polyPoints[i][0]) + (polyPoints[i][0] * polyPoints[i][1]) /
-        (polyPoints[j][0] - polyPoints[i][0]);
-      multiple[i] = (polyPoints[j][1] - polyPoints[i][1]) / (polyPoints[j][0] - polyPoints[i][0]);
-    }
-    j = i;
-  }
-  return [constant, multiple];
-};
-
-// find if one user is inside the polygon
-const isUserInPoints = (user, points, constant, multiple) => {
-  let j = points.length - 1;
-  let inside = true;
-  const userLocation = geo.decode(user);
-  for (let i = 0; i < points.length; i++) {
-    if ((points[i][0] < userLocation[0] && points[j][0] >= userLocation[0]
-      || points[j][0] < userLocation[0] && points[i][0] >= userLocation[0])) {
-      inside ^= (userLocation[0] * multiple[i] + constant[i] < userLocation[1]);
-    }
-    j = i;
-  }
-  return inside;
-};
-
-// find all the users inside the points of a certain polygon
-const getAllUsersInPoints = (points, users) => {
-  const affectedUsers = [];
-  const cashedPoly = cachePoly(points);
-  for (let i = 0; i < users.length; i += 2) {
-    if (isUserInPoints(users[i + 1], points, cashedPoly[0], cashedPoly[1])) {
-      affectedUsers.push(users[i]);
-    }
-  }
-  return affectedUsers;
-};
+const collision = require('../../lib/collision.js');
 
 const addPostToUser = (userId, post) => {
+  console.log(`adding post: ${post} to user: ${userId}`);
   const maxSavedPosts = 10;
   Promise.all([redisClient.saddAsync(userId, post), redisClient.scardAsync(userId)])
     .then((results) => {
-      console.log(results);
       // keep number of ripple posts under max limit
       if (results[1] > maxSavedPosts) {
         redisClient.spopAsync(userId, results[1] - maxSavedPosts);
@@ -119,18 +23,15 @@ const addPostToUser = (userId, post) => {
 };
 
 const processPost = (post, callback) => {
-  console.log('post currently being processed: ', post);
   // need the post's shape
   redisClient.hgetAsync('shapes', post.userId)
     .then((shape) => {
-      console.log('posts shape: ', shape);
       // need the post's lat & lng
       return Promise.all([shape, redisClient.zscoreAsync('locations', post.userId)]);
     })
     .then((results) => {
       // decode geohash to get post's lat lng
       const postLocation = geo.decode(results[1], 50);
-      console.log('post\'s location: ', postLocation);
 
       // send new ripple to redis queue so socket server can distribute
       // the post to all the connected sockets
@@ -142,12 +43,10 @@ const processPost = (post, callback) => {
       const shape = results[0].split('-')[1];
 
       // get all the points of the polygon in lat & lng
-      const shapePoints = getShapePoints(shape, postLocation, size);
-      console.log('post\'s points: ', shapePoints);
+      const shapePoints = collision.getShapePoints(shape, postLocation, size);
 
       // get the resolution we need to hash the post's lat and lng to
-      const resolution = getResolution(size);
-      console.log('post\'s resolution: ', resolution);
+      const resolution = geo.getResolution(size);
 
       // get the range of geocodes to get all near by users in a certain radius of the post
       let bottomRange = geo.hash(postLocation[0], postLocation[1], resolution);
@@ -159,22 +58,17 @@ const processPost = (post, callback) => {
         topRange *= 2;
       }
 
-      console.log('bottom range', bottomRange);
-      console.log('top range', topRange);
-
       return Promise.all([shapePoints,
         redisClient.zrangebyscoreAsync('locations', bottomRange, topRange, 'WITHSCORES')]);
     })
     .then((results) => {
       // from all the users in the area, find which ones are actually in the polygon
-      const affectedUsers = getAllUsersInPoints(results[0], results[1]);
-      callback(null, affectedUsers);
-      affectedUsers.forEach((user) => {
-        addPostToUser(user, post.postId);
-      });
+      const affectedUsers = collision.getAllUsersInPoints(results[0], results[1]);
+      callback(null, [affectedUsers, post.postId]);
     })
     .catch((err) => {
       console.error(err);
+      callback(err, null);
     });
 };
 
@@ -182,10 +76,10 @@ const processPostAsync = Promise.promisify(processPost);
 
 const workerJob = () => {
   process.on('message', (message) => {
-    console.log('recieved message from the master', message);
+    console.log(`recieved message from the master: ${message}`);
   });
 
-  // process posts one at a time
+  // process one post at a time
   const workerLoop = () => {
     redisClient.llenAsync('posts')
       .then((length) => {
@@ -198,8 +92,11 @@ const workerJob = () => {
               const task = JSON.parse(taskString);
               return processPostAsync(task);
             })
-            .then((users) => {
-              console.log('added post to these users list: ', users);
+            .then((affectedUsersAndPostId) => {
+              console.log('addeding post to these user\'s list: ', affectedUsersAndPostId);
+              affectedUsersAndPostId[0].forEach((user) => {
+                addPostToUser(user, affectedUsersAndPostId[1]);
+              });
               workerLoop();
             })
             .catch((err) => {
