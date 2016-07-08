1b4139df75a55418dd750fba64c0d9d9165ad27f
Leo Adelstein <leoadelstein@gmail.com>
2016-07-07 22:34:58 -0700

    added better error handling

--- server/httpServer/config/middleware.js
+++ server/httpServer/config/middleware.js
@@ -1,9 +1,16 @@
+
 const bodyParser = require('body-parser');
 const path = require('path');
 const morgan = require('morgan');
+
+const errorHandlers = require('./errorHandlers.js');
 
 module.exports = (app, express) => {
   app.use(morgan('dev'));
   app.use(bodyParser.json());
   app.use(express.static(path.resolve(`${__dirname}./../../../dist`)));
+
+  app.use(errorHandlers.logErrors);
+  app.use(errorHandlers.clientErrorHandler);
+  app.use(errorHandlers.errorHandler);
 };
